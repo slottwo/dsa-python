79e0e98d87390df5960a665ce65a696a2d0f4a3c
Pedro [slottwo] H. S. F. Galvão <41028091+slottwo@users.noreply.github.com>
2022-11-28 21:17:02 -0300

    Heapsort implemented

--- heap/heap.py
+++ heap/heap.py
@@ -1,24 +1,33 @@
 class Heap():
-    def __init__(self, *vector: int) -> None:
-        self._elem = list(vector)
-        for i in range(len(vector)//2, -1, -1):  # i from n//2 to 0
+    def __init__(self, *array: int) -> None:
+        self._elem = list(array)
+        # i from n//2 to 0
+        i = len(array)//2
+        while i >= 0:
             self.__down(i)
+            i -= 1
+
+    def __len__(self) -> int:
+        return len(self._elem)
+
+    def __swap(self, i, j):
+        self._elem[i], self._elem[j] = self._elem[j], self._elem[i]
 
     def __up(self, i: int):
         j = i//2  # round floor
         if j >= 0:
             if self._elem[i] > self._elem[j]:
-                self._elem[i], self._elem[j] = self._elem[j], self._elem[i]
+                self.__swap(i, j)
                 self.__up(j)
 
     def __down(self, i: int):
         j = 2*i
-        if j <= len(self._elem) - 1:
-            if j < len(self._elem) - 1:
+        if j <= len(self) - 1:
+            if j < len(self) - 1:
                 if self._elem[j] < self._elem[j+1]:
                     j = j+1
             if self._elem[i] < self._elem[j]:
-                self._elem[i], self._elem[j] = self._elem[j], self._elem[i]
+                self.__swap(i, j)
                 self.__down(j)
 
     def alter(self, i: int, value: int):
@@ -32,13 +41,20 @@ class Heap():
 
     def insert(self, value: int):
         self._elem.append(value)
-        n = len(self._elem)
-        self.__up(n-1)
+        self.__up(len(self) - 1)
 
-    def remove(self):
+    def remove(self) -> int:
+        removed = self._elem[0]
         self._elem[0] = self._elem[-1]  # copy the last one
         self._elem.pop(-1)  # remove it
         self.__down(0)
+        return removed
 
-    def Heapsort(self):
-        pass
+
+def Heapsort(array: list[int]) -> list[int]:
+    h = Heap(*array)
+    m = len(h) - 1
+    while m > 0:
+        array[m] = h.remove()
+        m -= 1
+    return array
