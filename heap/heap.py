9c968df01b06c38316f40731c428f4046ee51b9f
slottwo <41028091+slottwo@users.noreply.github.com>
2023-06-02 16:46:41 -0300

    Bugfix Heapsort

--- heap/heap.py
+++ heap/heap.py
@@ -67,7 +67,7 @@ class Heap():
 def Heapsort(array: list[int]) -> list[int]:
     h = Heap(*array)
     i = len(h) - 1
-    while i > 0:  # i from n - 1 to 0
+    while i > -1:  # i from n - 1 to 0
         array[i] = h.remove()
         i -= 1
     return array
