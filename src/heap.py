4cbdac088cbbf680cac98ad80f0a50e7fed170b0
slottwo <41028091+slottwo@users.noreply.github.com>
2023-06-19 19:49:29 -0300

    strange min heap

--- src/heap.py
+++ src/heap.py
@@ -1,9 +1,40 @@
+from typing import Iterator
+
+
 class MinHeap:
-    def __init__(self, **values) -> None:
-        ...
+    def __init__(H, *values) -> None:
+        H.__elements = list(values)
 
-    def pop(self) -> int:
-        ...
+    def __getitem__(H, __index: int) -> int:
+        return H.__elements[__index-1]
 
-    def update(self, index: int, value: any):
-        ...
+    def __setitem__(H, __index: int, __value: float):
+        H.__elements[__index-1] = __value
+
+    def __iter__(H) -> Iterator:
+        return iter(H.__elements)
+
+    def __up(H, __index: int) -> bool:
+        if __index > 1:
+            __lower_idx = __index / 2
+            if H[__lower_idx] > H[__index]:
+                H[__lower_idx], H[__index] = H[__lower_idx], H[__index]
+                H.__up(__lower_idx)
+                return True
+        return False
+
+    def __down(H, __index: int) -> bool:
+        __upper_idx = __index * 2
+        if __upper_idx < len(H):
+            if H[__upper_idx + 1] < H[__upper_idx]:
+                __upper_idx += 1
+            if H[__upper_idx] < H[__index]:
+                H[__upper_idx], H[__index] = H[__upper_idx], H[__index]
+                H.__down(__upper_idx)
+                return True
+        return False
+
+
+H = MinHeap(1, 2, 3, 4)
+H[0] = 5
+print(*H)
